a04169d5eb430bdf15640a51ef54a675d935ee14
THIVEND <baptiste.thivend@protonmail.com>
2021-06-15 15:45:05 +0200

    Clevercloud configuration

--- common/settings.py
+++ common/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = join(BASE_DIR, "static")
+
 
 STATIC_URL = '/static/'
 
