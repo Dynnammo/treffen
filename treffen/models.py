8f33b552afc343e7bafbd79539deb92d91d1d17b
THIVEND <baptiste.thivend@protonmail.com>
2021-06-16 19:22:57 +0200

    Add models for Player, Mission and Game

---
+++ treffen/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+
+class Player(models.Model):
+    name = models.CharField(
+        max_length=30,
+        blank=True
+    )
+    picture = models.ImageField(
+        upload_to='uploads/',
+
+    )
+    player_code = models.CharField(
+        max_length=10,
+        blank=True
+    )
+    ziel = models.OneToOneField(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='jaeger',
+        blank=True,
+        null=True
+    )
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE,
+        related_name='players'
+    )
+    mission = models.ForeignKey(
+        'Mission',
+        on_delete=models.CASCADE,
+        related_name='affected_players',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    GAME_MODES = [
+        ('BR', 'Battle Royale'),
+        ('CO', 'Cooperation'),
+        ('CH', 'Championship')
+    ]
+    name = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=GAME_MODES,
+        default='BR'
+    )
+    mission_set = models.ManyToManyField(
+        'Mission',
+        related_name='affected_games'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Mission(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+    description = models.TextField(
+        max_length=180
+    )
+
+    def __str__(self):
+        return self.name
