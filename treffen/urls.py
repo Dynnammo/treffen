fe06723d06f83bb3c815d18ff0bcd57132b335e6
THIVEND <baptiste.thivend@protonmail.com>
2021-06-18 20:24:03 +0200

    Create command for admin creation

--- treffen/urls.py
+++ treffen/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegistrationView, WaitingView
 
 urlpatterns = [
-    path('', RegistrationView.as_view(), name="registration_page"),
+    path('', RegistrationView.as_view(), name="registration"),
     path('waiting', WaitingView.as_view(), name="waiting_page")
 ]
