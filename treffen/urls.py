69c2e3dccc49313096b180485904c1c01bd34978
THIVEND <baptiste.thivend@protonmail.com>
2021-06-19 01:17:58 +0200

    Launch game

--- treffen/urls.py
+++ treffen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, WaitingView
+from .views import RegistrationView, WaitingView, GameView
 
 urlpatterns = [
     path('', RegistrationView.as_view(), name="registration"),
-    path('waiting', WaitingView.as_view(), name="waiting_page")
+    path('waiting', WaitingView.as_view(), name="waiting_page"),
+    path("game", GameView.as_view(), name="game")
 ]
--- treffen/views.py
+++ treffen/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import FormView, TemplateView
+from django.urls import reverse
+from django.shortcuts import redirect
 from .forms import RegistrationForm
 from .models import Player, Game
 
@@ -16,14 +18,33 @@ class RegistrationView(FormView):
             game=Game.objects.get(name=form.cleaned_data['game_name'])
         )
         response.set_cookie('player_id', p.id)
+        response.set_cookie('game_id', p.game.id)
         return response
 
 
 class WaitingView(TemplateView):
     template_name = "waiting.html"
 
+    def get(self, request, *args, **kwargs):
+        game = Game.objects.get(id=request.COOKIES['game_id'])
+        if game.status == 'S':
+            url = reverse('game')
+            return redirect(url)
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         player = Player.objects.get(id=self.request.COOKIES['player_id'])
         context["player"] = player
+        context["game_id"] = player.game.id
         return context
+
+
+class GameView(TemplateView):
+    template_name = "game.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["player"] = Player.objects.get(id=self.request.COOKIES['player_id'])
+        return context
+    
